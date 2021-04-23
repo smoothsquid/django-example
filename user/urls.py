6b000fe15eb9c2112500c10eea2b2ff8a35780d9
Jake Lee <jakeogtgg@gmail.com>
2021-04-23 23:53:58 +0900

    프로젝트 생성 및 사용자 기능 구현
    로그인, 회원가입, 로그아웃 적용

---
+++ user/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import (
+    SigninView,
+    SignoutView,
+    SignupView,
+)
+
+app_name = "user"
+
+urlpatterns = [
+    path("signup", SignupView.as_view(), name="signup"),
+    path("signout", SignoutView.as_view(), name="signout"),
+    path("signin", SigninView.as_view(), name="signin"),
+]
---
+++ user/views.py
@@ -0,0 +1,30 @@
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import auth
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse_lazy
+
+from .forms import SignupForm
+
+
+class SignupView(SuccessMessageMixin, CreateView):
+    template_name = "user/signup.html"
+    form_class = SignupForm
+    success_message = _("회원가입이 완료되었습니다.")
+    success_url = reverse_lazy("home")
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        if self.object:
+            auth.login(request, self.object)
+        return response
+
+
+class SignoutView(LogoutView):
+    next_page = reverse_lazy("home")
+
+
+class SigninView(LoginView):
+    template_name = "user/signin.html"
